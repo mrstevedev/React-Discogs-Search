e9beb41ce4654551568ec71bc8d116a5376dae18
Steven Pulido <spulido@phunware.com>
2019-02-17 11:17:26 -0800

    Updates

---
+++ precache-manifest.1f68b40043538c26557b08967ef11d60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7287087e237b63422f9",
+    "url": "/React-Discogs-Search/static/css/main.d3857eb5.chunk.css"
+  },
+  {
+    "revision": "e7287087e237b63422f9",
+    "url": "/React-Discogs-Search/static/js/main.e7287087.chunk.js"
+  },
+  {
+    "revision": "232b525ceda0cd6a06c9",
+    "url": "/React-Discogs-Search/static/js/1.232b525c.chunk.js"
+  },
+  {
+    "revision": "34782327f2bbf860e88d",
+    "url": "/React-Discogs-Search/static/js/runtime~main.34782327.js"
+  },
+  {
+    "revision": "e25ba34d7b1012bd3269ea139bd17f3e",
+    "url": "/React-Discogs-Search/static/media/logo.e25ba34d.svg"
+  },
+  {
+    "revision": "a60cc54012b29e6b6e48d8077e040ede",
+    "url": "/React-Discogs-Search/static/media/search.a60cc540.svg"
+  },
+  {
+    "revision": "18afd86e437dd9baba8922d32f434132",
+    "url": "/React-Discogs-Search/index.html"
+  }
+];
